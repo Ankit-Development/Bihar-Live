63dd7cd628cdd2b5f11f4a6ab50a37989731e5a3
Ankit <ankitkumardmk98@gmail.com>
2021-06-23 17:42:59 +0530

    1024830

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-(function ($) {
+(function($) {
     "use strict";
-    
+
     // Sticky Navbar
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 150) {
             $('.nav-bar').addClass('nav-sticky');
         } else {
             $('.nav-bar').removeClass('nav-sticky');
         }
     });
-    
-    
+
+
     // Dropdown on mouse hover
-    $(document).ready(function () {
+    $(document).ready(function() {
         function toggleNavbarMethod() {
             if ($(window).width() > 768) {
-                $('.navbar .dropdown').on('mouseover', function () {
+                $('.navbar .dropdown').on('mouseover', function() {
                     $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
+                }).on('mouseout', function() {
                     $('.dropdown-toggle', this).trigger('click').blur();
                 });
             } else {
@@ -27,22 +27,22 @@
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
-    
+
+
     // Back to top button
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    $('.back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-    
-    
+
+
     // Top News Slider
     $('.tn-slider').slick({
         autoplay: true,
@@ -51,8 +51,8 @@
         slidesToShow: 1,
         slidesToScroll: 1
     });
-    
-    
+
+
     // Category News Slider
     $('.cn-slider').slick({
         autoplay: false,
@@ -60,8 +60,7 @@
         dots: false,
         slidesToShow: 2,
         slidesToScroll: 1,
-        responsive: [
-            {
+        responsive: [{
                 breakpoint: 1200,
                 settings: {
                     slidesToShow: 2
@@ -87,8 +86,8 @@
             }
         ]
     });
-    
-    
+
+
     // Related News Slider
     $('.sn-slider').slick({
         autoplay: false,
@@ -96,8 +95,7 @@
         dots: false,
         slidesToShow: 3,
         slidesToScroll: 1,
-        responsive: [
-            {
+        responsive: [{
                 breakpoint: 1200,
                 settings: {
                     slidesToShow: 3
@@ -125,3 +123,69 @@
     });
 })(jQuery);
 
+
+
+
+// 
+// 
+function getAndUpdate() {
+    console.log("Updating List...");
+    tit = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+    if (localStorage.getItem('itemsJson') == null) {
+        itemJsonArray = [];
+        itemJsonArray.push([tit, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    } else {
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([tit, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    }
+    update();
+}
+
+function update() {
+    if (localStorage.getItem('itemsJson') == null) {
+        itemJsonArray = [];
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+    } else {
+        itemJsonArrayStr = localStorage.getItem('itemsJson')
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+    }
+    // Populate the table
+    let tableBody = document.getElementById("tableBody");
+    let str = "";
+    itemJsonArray.forEach((element, index) => {
+        str += `
+                    <tr>
+                    <th scope="row">${index + 1}</th>
+                    <td>${element[0]}</td>
+                    <td>${element[1]}</td> 
+                    <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td> 
+                    </tr>`;
+    });
+    tableBody.innerHTML = str;
+}
+add = document.getElementById("add");
+add.addEventListener("click", getAndUpdate);
+update();
+
+function deleted(itemIndex) {
+    console.log("Delete", itemIndex);
+    itemJsonArrayStr = localStorage.getItem('itemsJson')
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    // Delete itemIndex element from the array
+    itemJsonArray.splice(itemIndex, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    update();
+
+}
+
+function clearStorage() {
+    if (confirm("Do you really want to clear?")) {
+        console.log('Clearing the storage')
+        localStorage.clear();
+        update()
+    }
+}
